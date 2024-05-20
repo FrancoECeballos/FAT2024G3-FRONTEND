014076f1c1116724b6d72c14209dc097df4ab507
MateoTempesti <m.tempesti.ruffino@itsv.edu.ar>
2024-05-20 14:31:09 -0300

    log in card y nav

--- ProntaEntregaReact/src/App.jsx
+++ ProntaEntregaReact/src/App.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
 
 //importacion de los componentes
-import Login from '../src/components/Login'
 import './App.css'
 
 function App() {
   return (
   <div>
-    <Login/>
   </div>
   )
 }
