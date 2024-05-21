7e9715c7cb3354c5867fe2b3c241f97b0d7d1618
MateoTempesti <m.tempesti.ruffino@itsv.edu.ar>
2024-05-21 11:29:29 -0300

    log in modificado

--- ProntaEntregaReact/src/components/login/login_card/LoginCard.jsx
+++ ProntaEntregaReact/src/components/login/login_card/LoginCard.jsx
@@ -8,20 +8,20 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 const LoginCard = () => {
   return (
     <Container className="d-flex justify-content-center align-items-center vh-100">
-      <Card style={{ width: '35rem', height: '60vh' , borderRadius:'20px', boxShadow:'0px 5px 10px',}}>
+      <Card style={{ width: '35rem', height: '50vh' , borderRadius:'1rem', boxShadow:'0rem 2rem 5rem'}}>
         <Card.Body>
           <Form>
             <h1 style={{textAlign:"center", padding: ''}}>Log In</h1>
             <Form.Group className="mb-3" controlId="formBasicEmail">
               <Form.Label style={{display: 'block', marginBottom: ''}}>Email</Form.Label>
-              <Form.Control type="email" placeholder="Ingrese su email" />
+              <Form.Control style={{padding: '0.5rem', width: '15rem' }} type="email" placeholder="Ingrese su email" />
               <Form.Text className="text-muted">
               </Form.Text>
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formBasicPassword">
               <Form.Label>Contraseña</Form.Label>
-              <Form.Control type="password" placeholder="Ingrese su contraseña" />
+              <Form.Control style={{padding: '0.5rem', width: '15rem'}} type="password" placeholder="Ingrese su contraseña" />
             </Form.Group>
             <Form.Group className="mb-3" controlId="formBasicCheckbox">
               <Form.Check type="checkbox" label="Mantener sesion" />
@@ -30,6 +30,9 @@ const LoginCard = () => {
               Ingresar
             </Button>
           </Form>
+          <a href="register.html">¿No tienes cuenta?</a>
+          <br/>
+          <a href="">¿Olvidaste tu contraseña?</a>
         </Card.Body>
       </Card>
     </Container>
