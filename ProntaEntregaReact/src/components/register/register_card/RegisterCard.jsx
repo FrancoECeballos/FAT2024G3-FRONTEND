4c9b27f3952fac12f8fe5e3ceeeb30682efcf347
WeigandtTimoteo <weigandttimoteo@gmail.com>
2024-05-21 13:56:10 -0300

    Me frustre, pero se avanzo bastante

--- ProntaEntregaReact/src/components/register/register_card/RegisterCard.jsx
+++ ProntaEntregaReact/src/components/register/register_card/RegisterCard.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import Card from 'react-bootstrap/Card';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
@@ -8,6 +8,12 @@ import Col from 'react-bootstrap/Col';
 import InputGroup from 'react-bootstrap/InputGroup';
 
 const RegisterCard = () => {
+  const fileInputRef = useRef(null);
+
+  const handleFileButtonClick = () => {
+    fileInputRef.current.click();
+  };
+
   return (
     <>
       <style>
@@ -17,67 +23,85 @@ const RegisterCard = () => {
           .font-rubik {
             font-family: 'Rubik', sans-serif;
           }
+          
+          .hidden-file-input {
+            display: none;
+          }
         `}
       </style>
 
-      <Container className="d-flex justify-content-center align-items-center" style={{width: '50rem', borderRadius:'1rem', boxShadow:'0rem 2rem 5rem'}}>
-        <Card style={{width: '50rem' }}>
+      <Container className="d-flex justify-content-center align-items-center" style={{ marginTop: '7rem', width: '100%', maxWidth: '60rem', borderRadius: '1rem', boxShadow: '0 2rem 5rem rgba(0, 0, 0, 0.1)' }}>
+        <Card style={{ width: '100%' }}>
           <Card.Body>
             <Row>
-              <Col style={{ width: '35rem' }}>
+              <Col md={8}>
                 <Form>
                   <Form.Group className='mb-2' controlId='title'>
-                    <Form.Label className="font-rubik" style={{fontSize: '1.3rem'}}>Register:</Form.Label>
+                    <Form.Label className="font-rubik" style={{ fontSize: '1.3rem' }}>Register:</Form.Label>
                   </Form.Group>
 
                   <Form.Group className="mb-2" controlId="formBasicUser">
-                    <Form.Label className="font-rubik" style={{fontSize: '0.8rem'}}>Nombre de usuario</Form.Label>
-                    <Form.Control type="text" placeholder="Ingrese su nombre de usuario"/>
+                    <Form.Label className="font-rubik" style={{ fontSize: '0.8rem' }}>Nombre de usuario</Form.Label>
+                    <Form.Control type="text" placeholder="Ingrese su nombre de usuario" />
                   </Form.Group>
 
                   <Form.Group className="mb-2" controlId="formBasicEmail">
-                    <Form.Label className="font-rubik" style={{fontSize: '0.8rem'}}>Email</Form.Label>
-                    <Form.Control type="email" placeholder="Ingrese su email"/>
+                    <Form.Label className="font-rubik" style={{ fontSize: '0.8rem' }}>Email</Form.Label>
+                    <Form.Control type="email" placeholder="Ingrese su email" />
                   </Form.Group>
 
                   <Form.Group className="mb-2" controlId="formBasicDocumento">
-                    <Form.Label className="font-rubik" style={{fontSize: '0.8rem'}}>Documento de identidad</Form.Label>
+                    <Form.Label className="font-rubik" style={{ fontSize: '0.8rem' }}>Documento de identidad</Form.Label>
                     <InputGroup className="mb-2">
                       <Form.Control aria-label="Text input with dropdown button" />
                       <Form.Select aria-label="Default select example">
-                          <option>Seleccione un tipo de documento</option>
-                          <option value="1">DNI</option>
-                          <option value="2">Pasaporte</option>
-                          <option value="3">Carnet de extranjeria</option>
+                        <option>Seleccione un tipo de documento</option>
+                        <option value="1">DNI</option>
+                        <option value="2">Pasaporte</option>
+                        <option value="3">Carnet de extranjeria</option>
                       </Form.Select>
                     </InputGroup>
                   </Form.Group>
 
                   <Form.Group className="mb-2" controlId="formBasicGenero">
-                    <Form.Label className="font-rubik" style={{fontSize: '0.8rem'}}>Genero</Form.Label>
+                    <Form.Label className="font-rubik" style={{ fontSize: '0.8rem' }}>Genero</Form.Label>
                     <Form.Select aria-label="Default select example">
-                        <option>Seleccione un genero</option>
-                        <option value="1">Masculino</option>
-                        <option value="2">Femenino</option>
-                        <option value="3">Prefiero no decir</option>
+                      <option>Seleccione un genero</option>
+                      <option value="1">Masculino</option>
+                      <option value="2">Femenino</option>
+                      <option value="3">Prefiero no decir</option>
                     </Form.Select>
                   </Form.Group>
 
                   <Form.Group className="mb-2" controlId="formBasicPassword">
-                    <Form.Label className="font-rubik" style={{fontSize: '0.8rem'}}>Contraseña</Form.Label>
-                    <Form.Control type="password" placeholder="Ingrese su contraseña"/>
+                    <Form.Label className="font-rubik" style={{ fontSize: '0.8rem' }}>Contraseña</Form.Label>
+                    <Form.Control type="password" placeholder="Ingrese su contraseña" />
                   </Form.Group>
 
-                  <Form.Group className="mb-2" controlId="formBasicPassword">
-                    <Form.Label className="font-rubik" style={{fontSize: '0.8rem'}}>Confirmar contraseña</Form.Label>
-                    <Form.Control type="password" placeholder="Ingrese nuevamente su contraseña"/>
+                  <Form.Group className="mb-2" controlId="formBasicPasswordConfirm">
+                    <Form.Label className="font-rubik" style={{ fontSize: '0.8rem' }}>Confirmar contraseña</Form.Label>
+                    <Form.Control type="password" placeholder="Ingrese nuevamente su contraseña" />
                   </Form.Group>
                 </Form>
               </Col>
-              <Col className="d-flex flex-column justify-content-end align-items-end" style={{ width: '15rem' }}>
-                <Button variant="primary" type="submit">
-                  Ingresar
-                </Button>
+              <Col md={4} className="d-flex flex-column justify-content-end align-items-end">
+                <div className="flex-grow-1">
+                  <Form.Group className="mb-2" controlId="formFile">
+                    <input 
+                      type="file" 
+                      ref={fileInputRef} 
+                      className="hidden-file-input"
+                    />
+                    <Button variant="secondary" onClick={handleFileButtonClick}>
+                      Upload File
+                    </Button>
+                  </Form.Group>
+                </div>
+                <div className="d-flex justify-content-end w-100">
+                  <Button variant="primary" type="submit">
+                    Ingresar
+                  </Button>
+                </div>
               </Col>
             </Row>
           </Card.Body>
