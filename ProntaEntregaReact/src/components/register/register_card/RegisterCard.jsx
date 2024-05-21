42bd2dff8868df4713713ccdc75dfbba6728eb6a
WeigandtTimoteo <weigandttimoteo@gmail.com>
2024-05-21 08:34:21 -0300

    working on components

--- ProntaEntregaReact/src/components/register/register_card/RegisterCard.jsx
+++ ProntaEntregaReact/src/components/register/register_card/RegisterCard.jsx
@@ -6,17 +6,28 @@ import Button from 'react-bootstrap/Button';
 
 const LoginCard = () => {
   return (
+    <>
+    <style>
+        {`
+          @import url('https://fonts.googleapis.com/css2?family=Rubik:wght@400;500;700&display=swap');
+
+          .font-rubik {
+            font-family: 'Rubik', sans-serif;
+          }
+        `}
+    </style>
+
     <Container className="d-flex justify-content-center align-items-center" style={{ height: '100vh' }}>
       <Card style={{ width: '25rem' }}>
         <Card.Body>
           <Form>
             <Form.Group className='mb-3' controlId='title'>
-                <Form.Label>Register:</Form.Label>
+                <Form.Label className="font-rubik" style={{fontSize: '1.3rem'}}>Register:</Form.Label>
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Label>Email</Form.Label>
-              <Form.Control type="email" placeholder="Ingrese su email" />
+              <Form.Label className="font-rubik">Nombre de usuario</Form.Label>
+              <Form.Control type="text" />
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formBasicPassword">
@@ -35,6 +46,7 @@ const LoginCard = () => {
         </Card.Body>
       </Card>
     </Container>
+    </>
   );
 }
 
