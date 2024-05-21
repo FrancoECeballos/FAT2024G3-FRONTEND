072bb72ff8e8d2a6f3f00afcca35abf9f33bc32b
MateoTempesti <m.tempesti.ruffino@itsv.edu.ar>
2024-05-21 14:30:06 -0300

    log in reactivo

--- ProntaEntregaReact/src/components/login/login_card/LoginCard.jsx
+++ ProntaEntregaReact/src/components/login/login_card/LoginCard.jsx
@@ -8,13 +8,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 const LoginCard = () => {
   return (
     <Container className="d-flex justify-content-center align-items-center vh-100">
-      <Card style={{ width: '32rem', height: '55vh' , borderRadius:'1rem', boxShadow:'0rem 2rem 5rem'}}>
+      <Card style={{ width: '32rem',borderRadius:'1rem', boxShadow: '0 2rem 5rem rgba(0, 0, 0, 0.1)'}}>
         <Card.Body>
           <Form>
             <h1 style={{textAlign:"center",margin:'2.8rem'}}>Log In</h1>
             <Form.Group className="mb-3" controlId="formBasicEmail">
               <Form.Label style={{display: 'block', marginBottom: '0rem', marginLeft:'1.5rem'}}>Email</Form.Label>
-              <Form.Control style={{ width: '15rem', height: '4.5vh', borderRadius:'1rem', marginLeft:'1.5rem'}} type="email" placeholder="Ingrese su email" />
+              <Form.Control style={{ width: '15rem', height: '4.5vh', borderRadius:'1rem', marginLeft:'1.5rem', }} type="email" placeholder="Ingrese su email" />
               <Form.Text className="text-muted">
               </Form.Text>
             </Form.Group>
@@ -27,12 +27,12 @@ const LoginCard = () => {
               <Form.Check style={{marginTop: '1.5rem', marginLeft:'1.5rem'}} type="checkbox" label="Mantener sesion" />
             </Form.Group>
             <div style={{justifyContent: 'center', alignItems:'center',display: 'flex'}}>
-            <Button style={{borderRadius:'1rem',marginBottom: '1.5rem', marginTop: '1rem', width:'20rem', height: '5vh', textAlign:'center'}} variant="primary" type="submit">
+            <Button style={{borderRadius:'1rem', width:'20rem', height: '5vh', textAlign:'center'}} variant="primary" type="submit">
               Ingresar
             </Button>
             </div>
           </Form>
-          <div style={{ justifyContent: 'center', alignItems: 'center', display: 'flex', flexDirection: 'column' }}>
+          <div style={{ justifyContent: 'center', alignItems: 'center', display: 'flex', flexDirection: 'column', marginTop:'1rem' }}>
           <a href="register.html">¿No tienes cuenta?</a>
           <a href="">¿Olvidaste tu contraseña?</a>
           </div>
