a39653b4e37bc8f1b3813f7b16698af233897fc5
MateoTempesti <m.tempesti.ruffino@itsv.edu.ar>
2024-05-21 08:35:47 -0300

    nueva imagen nav

--- ProntaEntregaReact/src/components/login/login_nav/LoginNavbar.jsx
+++ ProntaEntregaReact/src/components/login/login_nav/LoginNavbar.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
-
+import Logo from '../../../assets/Logo.png';
 
 const LoginNavbar = () => {
     return (
         <Navbar id="login-navbar" expand="lg" className="bg-body-tertiary">
         <Container fluid>
-          <Navbar.Brand>Navbar scroll</Navbar.Brand>
+          <Navbar.Brand></Navbar.Brand>
+          <img src={Logo} alt='' id='logo' style={{ width: '4rem',}}/>
         </Container>
       </Navbar>
     );
