a5c1f0fe6772fe446c00ab49b770d1208c146f2c
FrancoECeballos <franco.ceballos2005@gmail.com>
2024-05-28 10:21:30 -0300

    Added .env file to connect to django

--- ProntaEntregaReact/src/pages/Register.jsx
+++ ProntaEntregaReact/src/pages/Register.jsx
@@ -1,8 +1,23 @@
 import React from 'react';
+import { useState, useEffect } from 'react'
 import RegisterCard from '../components/register/register_card/RegisterCard.jsx';
 import BaseNavbar from '../components/navbar/base_navbar/base_navbar.jsx';
 
 function Register (){
+  const url = 'http://localhost:8000';
+
+  useEffect(() => {
+    init()
+  } ,[])
+
+  const init = async() => {
+    let full_url = `${url}/user/`;
+    let result = await fetch(full_url);
+    result = await result.json();
+
+    console.log(`result`, result);
+  }
+
   return (
     <div>
       <BaseNavbar />
