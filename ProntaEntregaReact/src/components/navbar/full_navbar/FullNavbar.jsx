61542021da4b586f1e39b4906aee6f059b100758
WeigandtTimoteo <weigandttimoteo@gmail.com>
2024-05-30 11:57:50 -0300

    navbar en proceso

--- ProntaEntregaReact/src/components/navbar/full_navbar/FullNavbar.jsx
+++ ProntaEntregaReact/src/components/navbar/full_navbar/FullNavbar.jsx
@@ -1,5 +1,4 @@
-import React from "react";
-import { useState } from 'react';
+import React, { useState } from "react";
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -12,58 +11,59 @@ import NotifBell from '../../../assets/notification_bell.png';
 import UserInApp from '../../../assets/user_in_app.png';
 import SideBar from '../../../assets/side_bar.png';
 
-export default function FullNavbar({}) {
+export default function FullNavbar() {
 
 	const [show, setShow] = useState(false);
 
 	const handleClose = () => setShow(false);
 	const handleShow = () => setShow(true);
 
-    return (
-        <Navbar 
-            id="base-navbar" 
-            expand="sm" 
-            fixed="top" 
-            style={{ backgroundColor: '#A11818', color: 'white' }}
-        >
-            <Container fluid>
-                <Navbar.Brand>
-                    <a href="login.html">
-                        <img src={Logo} alt='Logo' id='logo' style={{ width: '4rem' }} />
-                    </a>
-                </Navbar.Brand>
+	return (
+		<Navbar 
+			id="base-navbar" 
+			expand="lg" 
+			fixed="top" 
+			style={{ backgroundColor: '#A11818', color: 'white' }}
+		>
+			<Container fluid>
+				<Navbar.Brand>
+					<a href="login.html">
+						<img src={Logo} alt='Logo' id='logo' style={{ width: '4rem' }} />
+					</a>
+				</Navbar.Brand>
 				<Nav className="me-auto">
 					<div className="pages-nav">
-					<div className="vertical-divider"></div>
-					<Nav.Link className="white" href="#home">Novedades</Nav.Link>
-					<div className="vertical-divider"></div>
-					<Nav.Link className="white" href="#link">Stock</Nav.Link>
-					<div className="vertical-divider"></div>
-					<Nav.Link className="white" href="#link">Entregas</Nav.Link>
-					<div className="vertical-divider"></div>
-					<Nav.Link className="white" href="#link">Pedidos</Nav.Link>
-					<div className="vertical-divider"></div>
-					<Nav.Link className="white" href="#link">Ofertas</Nav.Link>
-					<div className="vertical-divider"></div>
+						<div className="vertical-divider"></div>
+						<Nav.Link className="white" href="#home">Novedades</Nav.Link>
+						<div className="vertical-divider"></div>
+						<Nav.Link className="white" href="#link">Stock</Nav.Link>
+						<div className="vertical-divider"></div>
+						<Nav.Link className="white" href="#link">Entregas</Nav.Link>
+						<div className="vertical-divider"></div>
+						<Nav.Link className="white" href="#link">Pedidos</Nav.Link>
+						<div className="vertical-divider"></div>
+						<Nav.Link className="white" href="#link">Ofertas</Nav.Link>
+						<div className="vertical-divider"></div>
 					</div>
 				</Nav>
-				<Nav style={{width: '20%'}}>
-					<img src={NotifBell} style={{width: '2.5rem', marginRight:'5rem'}}/>
-					<img src={UserInApp} style={{width: '2.5rem', marginRight:'5rem'}}/>
-					<Button onClick={handleShow} style={{width: '2.5rem', paddingRight:'10rem'}}>
-						<img src={SideBar} style={{width:'2.5rem'}}/>
-					</Button>
-
-					<Offcanvas show={show} onHide={handleClose} backdrop="static">
-						<Offcanvas.Header closeButton>
-						<Offcanvas.Title>Offcanvas</Offcanvas.Title>
-						</Offcanvas.Header>
-						<Offcanvas.Body>
-						I will not close if you click outside of me.
-						</Offcanvas.Body>
-					</Offcanvas>
-				</Nav>
-            </Container>
-        </Navbar>
-    );
+				<div className="ml-auto d-lg-flex align-items-center">
+					<Nav>
+						<img src={NotifBell} style={{ width: '2.5rem', marginRight: '1rem' }}/>
+						<img src={UserInApp} style={{ width: '2.5rem', marginRight: '1rem' }}/>
+						<Button onClick={handleShow} style={{ width: '2.5rem' }}>
+							<img src={SideBar} style={{ width: '2.5rem' }}/>
+						</Button>
+						<Offcanvas show={show} onHide={handleClose} backdrop="static">
+							<Offcanvas.Header closeButton>
+								<Offcanvas.Title>Offcanvas</Offcanvas.Title>
+							</Offcanvas.Header>
+							<Offcanvas.Body>
+								I will not close if you click outside of me.
+							</Offcanvas.Body>
+						</Offcanvas>
+					</Nav>
+				</div>
+			</Container>
+		</Navbar>
+	);
 };
