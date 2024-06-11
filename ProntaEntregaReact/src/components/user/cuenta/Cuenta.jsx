35585a72b0d7590ae5f64aad6082ab7ca61a1154
FrancoECeballos <franco.ceballos2005@gmail.com>
2024-06-11 14:08:45 -0300

    Added handle logout

--- ProntaEntregaReact/src/components/user/cuenta/Cuenta.jsx
+++ ProntaEntregaReact/src/components/user/cuenta/Cuenta.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import fetchData from '../../../functions/fetchData';
 import Cookies from 'js-cookie';
 import Button from 'react-bootstrap/Button';
@@ -8,22 +8,21 @@ import './cuenta.scss';
 const Cuenta = () => {
     const navigate = useNavigate();
     const token = Cookies.get('token');
-    
 
     const [userData, setUserData] = useState({
-        "nombre": "",
-        "apellido": "",
-        "nombreusuario": "",
-        "password": "",
-        "documento": "",
-        "telefono": "",
-        "email": "",
-        "genero": "",
-        "id_direccion": "",
-        "id_tipousuario": "",
-        "id_tipodocumento": ""
-      });
-      
+        nombre: "",
+        apellido: "",
+        nombreusuario: "",
+        password: "",
+        documento: "",
+        telefono: "",
+        email: "",
+        genero: "",
+        id_direccion: "",
+        id_tipousuario: "",
+        id_tipodocumento: ""
+    });
+
     useEffect(() => {
         if (!token) {
             navigate('/login');
@@ -34,35 +33,39 @@ const Cuenta = () => {
         });
     }, [token]);
 
+    const handleLogout = () => {
+        Cookies.remove('token');
+        navigate('/login');
+    };
 
     return (
-    <div className='micuenta'>
-        <h1>Pedrito Me Electrocutaste</h1>
-        <form>
-            <div>
-                <label htmlFor="nombre">{`Nombre: ${userData.nombre}!`}</label>
-            </div>
-            <div>
-                <label htmlFor="apellido">{`Apellido: ${userData.apellido}!`}</label>
-            </div>
-            <div>
-                <label htmlFor="email">{`Email: ${userData.email}!`}</label>
-            </div>
-            <div>
-                <label htmlFor="telefono">{`Telefono: ${userData.telefono}!`}</label>
-            </div>
-            <div>
-                <label htmlFor="direccion">{`Direccion: ${userData.id_direccion}!`}</label>
-            </div>
-            <div>
-                <label htmlFor="genero">{`Genero: ${userData.genero}!`}</label>
-            </div>
-            <button type="submit">Editar</button>
-        </form>
-        <Button className="btn btn-danger" style={{marginTop: '15rem', marginLeft: '30rem'}} onClick={handleLogout}>
-            Logout
-        </Button>
-    </div>
+        <div className="micuenta">
+            <h1>Pedrito Me Electrocutaste</h1>
+            <form>
+                <div>
+                    <label htmlFor="nombre">{`Nombre: ${userData.nombre}`}</label>
+                </div>
+                <div>
+                    <label htmlFor="apellido">{`Apellido: ${userData.apellido}`}</label>
+                </div>
+                <div>
+                    <label htmlFor="email">{`Email: ${userData.email}`}</label>
+                </div>
+                <div>
+                    <label htmlFor="telefono">{`Telefono: ${userData.telefono}`}</label>
+                </div>
+                <div>
+                    <label htmlFor="direccion">{`Direccion: ${userData.id_direccion}`}</label>
+                </div>
+                <div>
+                    <label htmlFor="genero">{`Genero: ${userData.genero}`}</label>
+                </div>
+                <Button type="submit">Editar</Button>
+            </form>
+            <Button className="btn btn-danger" style={{ marginTop: '15rem', marginLeft: '30rem' }} onClick={handleLogout}>
+                Logout
+            </Button>
+        </div>
     );
 };
 
