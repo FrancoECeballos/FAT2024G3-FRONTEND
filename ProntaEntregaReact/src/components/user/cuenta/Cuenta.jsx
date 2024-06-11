25e75c22ef206468524dfa9a0977a067ae948938
MateoTempesti <m.tempesti.ruffino@itsv.edu.ar>
2024-06-11 11:25:39 -0300

    cuenta

--- ProntaEntregaReact/src/components/user/cuenta/Cuenta.jsx
+++ ProntaEntregaReact/src/components/user/cuenta/Cuenta.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import './cuenta.scss';
 
 const Cuenta = () => {
     return (
-    <div>
-      
+    <div className='micuenta'>
+      <h1>Pedrito Me Electrocutaste</h1>
+      <form>
+        <div>
+          <label htmlFor="nombre">Nombre:</label>
+        </div>
+        <div>
+          <label htmlFor="apellido">Apellido:</label>
+        </div>
+        <div>
+          <label htmlFor="email">Email:</label>
+        </div>
+        <div>
+          <label htmlFor="telefono">Telefono:</label>
+        </div>
+        <div>
+          <label htmlFor="direccion">Direccion:</label>
+        </div>
+        <div>
+          <label htmlFor="genero">Genero:</label>
+        </div>
+        <button type="submit">Editar</button>
+      </form>
     </div>
     );
 };
