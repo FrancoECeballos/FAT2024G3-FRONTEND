0ef34c34963a0494d327b44ec6fdb8c686a69454
MateoTempesti <m.tempesti.ruffino@itsv.edu.ar>
2024-06-13 10:12:11 -0300

    creacion de lasa columnas en el perfil

--- ProntaEntregaReact/src/components/user/cuenta/Cuenta.jsx
+++ ProntaEntregaReact/src/components/user/cuenta/Cuenta.jsx
@@ -44,34 +44,36 @@ const Cuenta = () => {
     };
 
     return (
-    <div className='micuenta'>
-      <h1>{`Bienvenido ${userData.nombreusuario}`}</h1>
-      <form>
-        <h2>Información personal</h2>
-        <div>
-            <label for="nombre">Nombre:</label>
-            <input type="text" id="nombre" value={`${userData.nombre}`} disabled/>
+<div class="micuenta">
+  <h1>{`Bienvenido ${userData.nombreusuario}`}</h1>
+  <h2>Información personal</h2>
+  <form>
+    <div class="contenedor-inputs">
+      <div class="columna">
+        <label for="nombre">Nombre:</label>
+        <input type="text" id="nombre" value={`${userData.nombre}`} disabled />
 
-            <label for="apellido">Apellido:</label>
-            <input type="text" id="apellido" value={`${userData.apellido}`} disabled/>
+        <label for="apellido">Apellido:</label>
+        <input type="text" id="apellido" value={`${userData.apellido}`} disabled />
 
-            <label for="email">Correo electrónico:</label>
-            <input type="email" id="email" value={`${userData.email}`} disabled/>
+        <label for="email">Correo electrónico:</label>
+        <input type="email" id="email" value={`${userData.email}`} disabled />
+      </div>
 
-            <label for="telefono">Teléfono:</label>
-            <input type="tel" id="telefono" value={`${userData.telefono}`} disabled/>
+      <div class="columna">
+        <label for="telefono">Teléfono:</label>
+        <input type="tel" id="telefono" value={`${userData.telefono}`} disabled />
 
-            <label for="direccion">Dirección:</label>
-            <input type="text" id="direccion" value={`${userData.id_direccion.localidad}, ${userData.id_direccion.calle} ${userData.id_direccion.numero}`} disabled/>
-                
-            <label for="genero">Genero:</label>
-            <input type="text" id="genero" value={`${GENDER_CHOICES[userData.genero]}`} disabled/>
-            
-            <label for="fechaNacimiento">Fecha de nacimiento:</label>
-            <input type="date" id="fechaNacimiento" disabled/>
-        </div>
-      </form>
+        <label for="direccion">Dirección:</label>
+        <input type="text" id="direccion" value={`${userData.id_direccion.localidad}, ${userData.id_direccion.calle} ${userData.id_direccion.numero}`} disabled />
+
+        <label for="genero">Genero:</label>
+        <input type="text" id="genero" value={`${GENDER_CHOICES[userData.genero]}`} disabled />
+      </div>
     </div>
+  </form>
+</div>
+
     );
 };
 
