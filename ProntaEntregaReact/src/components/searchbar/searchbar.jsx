e21965db4a51b860883b426399e03c739c6cf82c
WeigandtTimoteo <weigandttimoteo@gmail.com>
2024-06-13 12:00:22 -0300

    searchbar iniciada

--- ProntaEntregaReact/src/components/searchbar/searchbar.jsx
+++ ProntaEntregaReact/src/components/searchbar/searchbar.jsx
@@ -1,10 +1,21 @@
 import React from "react";
+import { Dropdown } from 'react-bootstrap';
 
 function SearchBar() {
     return (
-        <div>
-        <input type="text" placeholder="Search.." name="search" />
-        <button type="submit">Search</button>
+        <div style={{display:'flex', marginLeft: '3rem'}}>
+        <input type="text" placeholder="Search.." name="search" style={{width: '60rem', marginRight: '2rem'}}/>
+        <Dropdown>
+            <Dropdown.Toggle variant="success" id="dropdown-basic">
+                Dropdown Button
+            </Dropdown.Toggle>
+
+            <Dropdown.Menu>
+                <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+                <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+            </Dropdown.Menu>
+        </Dropdown>
         </div>
     );
 }
