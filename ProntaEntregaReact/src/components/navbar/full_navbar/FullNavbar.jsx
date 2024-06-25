cee73918d5c0475b87439b7b36deba0d5c71fd97
RenzoProsdo <renzo.prosdocimo.ps4@gmail.com>
2024-06-25 10:54:21 -0300

    new navbar

--- ProntaEntregaReact/src/components/navbar/full_navbar/FullNavbar.jsx
+++ ProntaEntregaReact/src/components/navbar/full_navbar/FullNavbar.jsx
@@ -1,8 +1,10 @@
-import React, { useEffect, useState } from 'react';
-import { Container, Button, Nav, Navbar, Offcanvas } from 'react-bootstrap';
-import { useNavigate } from 'react-router-dom';
-import Cookies from 'js-cookie';
-
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Offcanvas from 'react-bootstrap/Offcanvas';
 import fetchData from '../../../functions/fetchData';
 
 import logo from '../../../assets/Logo.png';
@@ -13,82 +15,98 @@ import user from '../../../assets/user_in_app.png';
 import './FullNavbar.scss';
 
 function FullNavbar() {
+  const expand = false; 
   const navigate = useNavigate();
   const [show, setShow] = useState(false);
   const [data, setData] = useState([]);
   const token = Cookies.get('token');
 
   useEffect(() => {
-    const fetchUserData = async () => {
-      if (token) {
-        const result = await fetchData(`/userToken/${token}`);
-        setData(result);
-      }
-    };
-    fetchUserData();
-    console.log(data);
+  const fetchUserData = async () => {
+    if (token) {
+      const result = await fetchData(`/userToken/${token}`);
+      setData(result);
+    }
+  };
+  fetchUserData();
+  console.log(data);
   }, []);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
   const handleSuperUserAuth = async (e) => {
-    e.preventDefault();
-    if (data.is_superuser) {
-      navigate('/selectuser');
-    } else if (token) {
-      navigate('/perfil/micuenta');
-    }
-    else {
-      navigate('/login');
-    }
+  e.preventDefault();
+  if (data.is_superuser) {
+    navigate('/selectuser');
+  } else if (token) {
+    navigate('/perfil/micuenta');
+  }
+  else {
+    navigate('/login');
+  }
   };
 
-  return (
-    <Navbar expand={true} className="full-navbar" id="base-navbar">
-      <Container>
-        <div className='botons-izq'>
-          <Navbar.Brand>
-            <img src={logo} alt="Logo" className="logo" onClick={() => navigate('/')} />
-          </Navbar.Brand>
-        </div>
-        <Nav>
-          <div className="vertical-divider"></div>
-          <Nav.Link className="nav-link" style={{ alignContent: 'center' }}>Novedades</Nav.Link>
-          <div className="vertical-divider"></div>
-          <Nav.Link className="nav-link" style={{ alignContent: 'center' }} onClick={() => navigate('/stock')}>Stock</Nav.Link>
-          <div className="vertical-divider"></div>
-          <Nav.Link className="nav-link" style={{ alignContent: 'center' }}>Entregas</Nav.Link>
-          <div className="vertical-divider"></div>
-          <Nav.Link className="nav-link" style={{ alignContent: 'center' }}>Pedidos</Nav.Link>
-          <div className="vertical-divider"></div>
-          <Nav.Link className="nav-link" style={{ alignContent: 'center' }}>Ofertas</Nav.Link>
-          <div className="vertical-divider"></div>
-        </Nav>
-        <div className='botons-derecha'>
-          <Navbar.Brand>
-            <img src={noti} alt="Noti" className="noti" style={{ width: '3rem' }} />
-          </Navbar.Brand>
-          <Navbar.Brand>
-            <img src={user} alt="User" className="user" style={{ width: '3rem' }} onClick={handleSuperUserAuth} />
-          </Navbar.Brand>
-          <Button className="side-button" onClick={handleShow}>
-            <img src={side} alt="side" className="side-icon" />
-          </Button>
-        </div>
 
-        <Offcanvas show={show} onHide={handleClose} placement="end" style={{ backgroundColor: '#A11818', color: 'white' }}>
-          <Offcanvas.Header closeButton>
-            <Offcanvas.Title>Offcanvas</Offcanvas.Title>
+
+  return (
+    <Navbar expand={expand} className="full-navbar ">
+      <Container fluid>
+      <div>
+        <Navbar.Brand href="#">
+          <img src={logo} alt="Logo" className="logo" onClick={() => navigate('/')} />
+          </Navbar.Brand>  
+          </div>
+          
+          <Nav className="nav-link">
+          <div className="vertical-divider"></div>
+          <Nav.Link style={{ color: 'white' }}>Novedades</Nav.Link>
+          <div className="vertical-divider"></div>
+          <Nav.Link onClick={() => navigate('/stock')} style={{ color: 'white' }} >Stock</Nav.Link>
+          <div className="vertical-divider"></div>
+          <Nav.Link style={{ color: 'white' }} > Entregas</Nav.Link>
+          <div className="vertical-divider"></div>
+          <Nav.Link style={{ color: 'white' }} > Pedidos</Nav.Link>
+          <div className="vertical-divider"></div>
+          <Nav.Link style={{ color: 'white' }} > Ofertas</Nav.Link>
+          <div className="vertical-divider"></div>
+          </Nav>
+          <div className='botons-derecha'>        
+            <Navbar.Brand href="#">
+        <img src={noti} alt="Noti" className="noti" style={{ width: '3rem' }} />
+          </Navbar.Brand>  
+          <Navbar.Brand href="#">
+          <img src={user} alt="User" className="user" style={{ width: '3rem' }} onClick={handleSuperUserAuth} />
+          </Navbar.Brand>  
+        
+        
+        <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
+        <Navbar.Offcanvas
+          id={`offcanvasNavbar-expand-${expand}`}
+          aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+          placement="end"
+        >
+          <Offcanvas.Header closeButton style={{ backgroundColor: '#A11818', color: 'white' }}> 
+            <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
+              Offcanvas
+            </Offcanvas.Title>
           </Offcanvas.Header>
-          <Offcanvas.Body>
+          <Offcanvas.Body style={{ backgroundColor: '#A11818', color: 'white' }}>
+            <Nav className="justify-content-end flex-grow-1 pe-3">
             <Nav.Link style={{ color: 'white' }}>Novedades</Nav.Link>
             <Nav.Link style={{ color: 'white' }}>Stock</Nav.Link>
             <Nav.Link style={{ color: 'white' }}>Entregas</Nav.Link>
             <Nav.Link style={{ color: 'white' }}>Pedidos</Nav.Link>
             <Nav.Link style={{ color: 'white' }}>Ofertas</Nav.Link>
+ 
+            </Nav>
+            
+            <Form className="d-flex">
+            
+            </Form>
           </Offcanvas.Body>
-        </Offcanvas>
+        </Navbar.Offcanvas>
+        </div>
       </Container>
     </Navbar>
   );
