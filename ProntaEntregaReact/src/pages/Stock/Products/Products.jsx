b917994acc5c9a7e05297bbbfcd252d48d0c5edf
FrancoECeballos <franco.ceballos2005@gmail.com>
2024-07-26 09:25:44 -0300

    Añadido total de un producto

--- ProntaEntregaReact/src/pages/Stock/Products/Products.jsx
+++ ProntaEntregaReact/src/pages/Stock/Products/Products.jsx
@@ -57,6 +57,10 @@ function Products() {
                 console.error('Error fetching products:', error);
             }
         };
+
+        fetchData(`unidad_medida/`, token).then((result) => {
+            setUnidadMedida(result);
+        });
     
         fetchProducts();
     }, [token, navigate, stockId, categoriaID]);
@@ -124,59 +128,63 @@ function Products() {
             <FullNavbar />
             <div className='margen-arriba'>
                 <SearchBar onSearchChange={handleSearchChange} onOrderChange={setOrderCriteria} filters={filters}/>
-                {Array.isArray(sortedProducts) && sortedProducts.map(product => (
-                    <AcordeonCard
-                        key={product.id_producto}
-                        titulo={product.nombre}
-                        acordeonTitle={`Ver almacén de: ${product.nombre}`}
-                        descrip1={product.descripcion}
-                        /*descrip2={`Cantidad: ${product.cantidad} ${product.id_unidadmedida.nombre}`}*/
-                        children={
-                            <Modal openButtonText="Modificar Stock" handleShowModal={() => newDetail(product.id_producto)} handleCloseModal={() => resetDetail()} title="Modificar Stock" saveButtonText="Guardar" handleSave={() => handleSave(product.id_detallestockproducto)}
-                            content={
-                                <div>
-                                    <h2 className='centered'> Producto: {product.id_producto.nombre} </h2>
-                                    <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-                                        <Form.Label style={{ marginLeft: '1rem' }}>Cantidad</Form.Label>
-                                        {isPaquete && (
-                                            <Form.Label style={{ marginRight: '1rem' }}>Cantidad/Paquetes</Form.Label>
-                                        )}
+                {Array.isArray(sortedProducts) && sortedProducts.map(product => {
+                    const totalMultiplicacion = product.id_detalle ? product.id_detalle.reduce((sum, detail) => sum + detail.multiplicacion, 0) : 0;
+
+                    return (
+                        <AcordeonCard
+                            key={product.id_producto}
+                            titulo={product.nombre}
+                            acordeonTitle={`Ver almacén de: ${product.nombre}`}
+                            descrip1={product.descripcion}
+                            descrip2={`Total: ${totalMultiplicacion} ${product.id_unidadmedida.identificador}`}
+                            children={
+                                <Modal openButtonText="Modificar Stock" handleShowModal={() => newDetail(product.id_producto)} handleCloseModal={() => resetDetail()} title="Modificar Stock" saveButtonText="Guardar" handleSave={() => handleSave(product.id_detallestockproducto)}
+                                content={
+                                    <div>
+                                        <h2 className='centered'> Producto: {product.id_producto.nombre} </h2>
+                                        <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                                            <Form.Label style={{ marginLeft: '1rem' }}>Cantidad</Form.Label>
+                                            {isPaquete && (
+                                                <Form.Label style={{ marginRight: '1rem' }}>Cantidad/Paquetes</Form.Label>
+                                            )}
+                                        </div>
+                                        <InputGroup className="mb-2">
+                                            <Form.Control name="cantidad" type="number" ref={cantidadRef} onChange={fetchSelectedObject} 
+                                                style={!isPaquete ? { borderRadius: '10rem', backgroundColor: '#F5F5F5', boxShadow: '0.10rem 0.3rem 0.20rem rgba(0, 0, 0, 0.3)', marginTop: '1rem' } : null}
+                                                className={isPaquete ? "unified-input-left" : null}/>
+                                            {isPaquete && (
+                                                <Form.Control name="cantidadUnidades" type="number" ref={cantidadUnidadesRef} className="unified-input-right" onChange={fetchSelectedObject}/>
+                                            )}
+                                        </InputGroup>
+                                        <Form.Select name="id_unidadMedida" ref={unidadMedidaRef} onChange={fetchSelectedObject} style={{ borderRadius: '10rem', backgroundColor: '#F5F5F5', boxShadow: '0.10rem 0.3rem 0.20rem rgba(0, 0, 0, 0.3)', marginTop: '1rem' }}>
+                                            <option autoFocus hidden>Seleccionar...</option>
+                                            {unidadMedida.map((item) => (
+                                                <option key={item.id} value={item.id}>{item.nombre}</option>
+                                            ))}
+                                        </Form.Select>
                                     </div>
-                                    <InputGroup className="mb-2">
-                                        <Form.Control name="cantidad" type="number" ref={cantidadRef} onChange={fetchSelectedObject} 
-                                            style={!isPaquete ? { borderRadius: '10rem', backgroundColor: '#F5F5F5', boxShadow: '0.10rem 0.3rem 0.20rem rgba(0, 0, 0, 0.3)', marginTop: '1rem' } : null}
-                                            className={isPaquete ? "unified-input-left" : null}/>
-                                        {isPaquete && (
-                                            <Form.Control name="cantidadUnidades" type="number" ref={cantidadUnidadesRef} className="unified-input-right" onChange={fetchSelectedObject}/>
-                                        )}
-                                    </InputGroup>
-                                    <Form.Select name="id_unidadMedida" type="text" ref={unidadMedidaRef} onChange={fetchSelectedObject} style={{ borderRadius: '10rem', backgroundColor: '#F5F5F5', boxShadow: '0.10rem 0.3rem 0.20rem rgba(0, 0, 0, 0.3)', marginTop: '1rem' }}>
-                                        <option autoFocus hidden>Seleccionar...</option>
-                                        {unidadMedida.map((item) => (
-                                            <option key={item.id} value={item.id}>{item.nombre}</option>
-                                        ))}
-                                    </Form.Select>
-                                </div>
-                            }/>
-                        }
-                        accordionChildren={
-                            Array.isArray(product.id_detalle) && product.id_detalle.map(detail => {
-                                return (
-                                    <LittleCard
-                                        key={detail.id_detallestockproducto}
-                                        foto={detail.id_producto.foto}
-                                        titulo={detail.id_unidadmedida.nombre}
-                                        descrip1={
-                                            detail.id_unidadmedida.paquete 
-                                            ? `Cantidad: ${detail.cantidad} ${detail.id_unidadmedida.identificador} ${detail.cantidadUnidades}` 
-                                            : `Cantidad: ${detail.cantidad} ${detail.id_unidadmedida.identificador}`
-                                        }
-                                    />
-                                );
-                            })
-                        }
-                    />
-                ))}
+                                }/>
+                            }
+                            accordionChildren={
+                                Array.isArray(product.id_detalle) && product.id_detalle.map(detail => {
+                                    return (
+                                        <LittleCard
+                                            key={detail.id_detallestockproducto}
+                                            foto={detail.id_producto.foto}
+                                            titulo={detail.id_unidadmedida.nombre}
+                                            descrip1={
+                                                detail.id_unidadmedida.paquete 
+                                                ? `Cantidad: ${detail.cantidad} ${detail.id_unidadmedida.identificador} ${detail.cantidadUnidades}` 
+                                                : `Cantidad: ${detail.cantidad} ${detail.id_unidadmedida.identificador}`
+                                            }
+                                        />
+                                    );
+                                })
+                            }
+                        />
+                    );
+                })}
             </div>
         </div>
     );
