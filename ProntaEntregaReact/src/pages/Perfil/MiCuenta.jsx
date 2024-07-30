3f8157854c9d91ff5056286175c796cb726ae015
RenzoProsdo <renzo.prosdocimo.ps4@gmail.com>
2024-07-30 12:52:29 -0300

    userpage

--- ProntaEntregaReact/src/pages/Perfil/MiCuenta.jsx
+++ ProntaEntregaReact/src/pages/Perfil/MiCuenta.jsx
@@ -2,13 +2,18 @@ import React from 'react';
 import Cuenta from '../../components/user/cuenta/Cuenta';
 import FullNavbar from '../../components/navbar/full_navbar/FullNavbar';
 import Sidebar from '../../components/user/sidebar/Sidebar_perfil';
+import { Col } from 'react-bootstrap'
 
 function MiCuenta(){
     return (
         <div style={{backgroundColor: '#ECECEC'}}>
-            <Sidebar/>
             <FullNavbar/>
-            <Cuenta/>
+            <Col xs={12} sm={4} md={4} lg={4} xl={4} xxl={4}>
+                <Sidebar/>
+            </Col>
+            <Col>
+                <Cuenta/>
+            </Col>
         </div>
     );
 };
