567807908c34a1f2376ea31a982042a586c20acc
gonzalomartinex <gonzalowmartinez@gmail.com>
2024-08-01 10:25:32 -0300

    imagen para casas

--- ProntaEntregaReact/src/components/cards/generic_card/GenericCard.jsx
+++ ProntaEntregaReact/src/components/cards/generic_card/GenericCard.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { Card, Row, Col } from 'react-bootstrap';
+import noImg from '../../../assets/no_image.png';
+
 
 function GenericCard({foto, titulo, descrip1, descrip2, descrip3, descrip4, descrip5, children, onClick}) {
     return (
@@ -7,7 +9,7 @@ function GenericCard({foto, titulo, descrip1, descrip2, descrip3, descrip4, desc
             <Card onClick={onClick} style={{ width: '90%', borderRadius: '1rem', boxShadow: '0.10rem 0.3rem 0.20rem rgba(0, 0, 0, 0.3)', padding: '0.5rem' }}>
                 <Row className="g-0" style={{padding: '1rem', display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}}>
                     <Col xs={4} md={2} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                        <img src={foto} style={{ width: '100%', borderRadius: '1rem' }} />
+                        <img src={foto || noImg} style={{ width: '100%', borderRadius: '1rem' }} />
                     </Col>
                     <Col xs={6} md={8}>
                         <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', height: '100%', marginLeft: '3rem' }}>
