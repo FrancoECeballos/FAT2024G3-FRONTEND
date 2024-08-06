e93959cd22170c246833939d51f63a09e4ba44e8
RenzoProsdo <renzo.prosdocimo.ps4@gmail.com>
2024-08-06 12:13:38 -0300

    generic card responsive

--- ProntaEntregaReact/src/components/cards/generic_card/GenericCard.jsx
+++ ProntaEntregaReact/src/components/cards/generic_card/GenericCard.jsx
@@ -2,19 +2,39 @@ import React from "react";
 import { Card, Row, Col } from 'react-bootstrap';
 import noImg from '../../../assets/no_image.png';
 
-
 function GenericCard({ foto, titulo, descrip1, descrip2, descrip3, descrip4, descrip5, children, onClick, cardStyle, imageStyle }) {
     return (
         <div style={{ display: 'flex', justifyContent: 'center', margin: '1rem' }}>
-            <Card onClick={onClick} style={{ ...cardStyle, width: '90%', borderRadius: '1rem', boxShadow: '0.10rem 0.3rem 0.20rem rgba(0, 0, 0, 0.3)', padding: '0.5rem' }}>
-                <Row className="g-0" style={{ padding: '1rem', display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
-                    <Col xs={4} md={2} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                        <img src={foto || noImg} style={{ ...imageStyle, width: '100%', borderRadius: '1rem', maxHeight: '18rem' }} />
+            <Card 
+                onClick={onClick} 
+                style={{ 
+                    ...cardStyle, 
+                    width: '100%', 
+                    maxWidth: '800px', 
+                    borderRadius: '1rem', 
+                    boxShadow: '0.10rem 0.3rem 0.20rem rgba(0, 0, 0, 0.3)', 
+                    padding: '0.5rem' 
+                }}
+            >
+                <Row className="g-0">
+                    <Col xs={12} md={3} className="d-flex justify-content-center align-items-center">
+                        <img 
+                            src={foto || noImg} 
+                            alt="Card" 
+                            style={{ 
+                                ...imageStyle, 
+                                width: '100%', 
+                                height: 'auto', 
+                                borderRadius: '1rem', 
+                                maxHeight: '18rem', 
+                                objectFit: 'cover' 
+                            }} 
+                        />
                     </Col>
-                    <Col xs={6} md={8}>
-                        <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', height: '100%', marginLeft: '3rem' }}>
+                    <Col xs={12} md={7} className="d-flex align-items-center">
+                        <div className="p-3">
                             <h3>{titulo}</h3>
-                            <div style={{ marginLeft: '1rem' }}>
+                            <div>
                                 <p>{descrip1}</p>
                                 <p>{descrip2}</p>
                                 <p>{descrip3}</p>
@@ -22,8 +42,8 @@ function GenericCard({ foto, titulo, descrip1, descrip2, descrip3, descrip4, des
                                 <p>{descrip5}</p>
                             </div>
                         </div>
-                    </Col>
-                    <Col xs={2} md={2} style={{ display: 'flex', justifyContent: 'flex-end', alignItems: 'center' }}>
+                    </Col> 
+                    <Col xs={12} md={2} className="d-flex justify-content-end align-items-center">
                         {children}
                     </Col>
                 </Row>
