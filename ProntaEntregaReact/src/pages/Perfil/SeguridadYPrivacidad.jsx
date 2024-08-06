bccacb5b68526fd807d4c49997210384ea954eb4
MateoTempesti <m.tempesti.ruffino@itsv.edu.ar>
2024-08-06 10:28:25 -0300

    modificacion de mi cuenta y el stock

--- ProntaEntregaReact/src/pages/Perfil/SeguridadYPrivacidad.jsx
+++ ProntaEntregaReact/src/pages/Perfil/SeguridadYPrivacidad.jsx
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import Cookies from 'js-cookie';
 import Seguridad from '../../components/user/seguridad/Seguridad';
 import FullNavbar from '../../components/navbar/full_navbar/FullNavbar';
 import Sidebar from '../../components/user/sidebar/Sidebar_perfil';
+import fetchData from '../../functions/fetchData.jsx';
 
 function SeguridadYPrivacidad(){
+    const token = Cookies.get('token');
+    const [user, setUser] = useState({});
+    useEffect(() => {
+        fetchData(`/userToken/${token}`).then((result) => {
+            setUser(result);
+        });
+    }, [token]);
     return (
         <div style={{backgroundColor: '#ECECEC'}}>
-            <Sidebar/>
             <FullNavbar/>
+            <Sidebar isAdmin={user.is_staff}/>
             <Seguridad/>
         </div>
     );
