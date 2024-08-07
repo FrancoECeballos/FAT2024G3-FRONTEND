857f9c1d96acc6d219b1921de3ba4570a133e02a
WeigandtTimoteo <weigandttimoteo@gmail.com>
2024-08-07 08:26:33 -0300

    generic table creada

---
+++ ProntaEntregaReact/src/components/tables/generic_table/generic_table.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+import { Table } from "react-bootstrap";
+
+
+const GenericTable = ({ data, headers }) => {
+  return (
+    <Table striped bordered hover>
+      <thead>
+        <tr>
+          {headers.map((header, index) => (
+            <th key={index}>{header}</th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((row, index) => (
+          <tr key={index}>
+            {row.map((cell, index) => (
+              <td key={index}>{cell}</td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </Table>
+  );
+}; export default GenericTable;
--- ProntaEntregaReact/src/pages/Novedades/Novedades.jsx
+++ ProntaEntregaReact/src/pages/Novedades/Novedades.jsx
@@ -1,33 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import FullNavbar from '../../components/navbar/full_navbar/FullNavbar.jsx';
-import GenericCard from '../../components/cards/generic_card/GenericCard.jsx';
+import GenericTable from '../../components/tables/generic_table/generic_table.jsx';
 
 import fetchData from '../../functions/fetchData.jsx';
 
 
 const Novedades = () => {
-    const [producto, setProducto] = useState(null);
-
-    useEffect(() => {
-        fetchData('/producto/1/')
-            .then(data => {
-                setProducto(data);
-                console.log(data);
-            })
-            .catch(error => console.error(error));
-    }, []);
-
     return (
         <div>
-            <FullNavbar />
-            {producto && (
-                <GenericCard
-                    titulo={producto[0].nombre}
-                    foto={`${producto[0].imagen}`} // Asumiendo que 'imagen' es el campo con la ruta de la imagen
-                    descrip1={producto[0].descripcion} // Ajusta según los campos disponibles en tu producto
-                    descrip2={producto[0].imagen}
-                />
-            )}
+            <FullNavbar/>
+            <GenericTable
+            />
         </div>
     );
 };
