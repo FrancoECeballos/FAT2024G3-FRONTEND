beb37a3436f4250f258f82a447bf58c6c43cb4b0
Lucio Peralta <lucio.peralta.ar@gmail.com>
2024-08-13 10:30:10 -0300

    no se

--- ProntaEntregaReact/src/pages/Autos/Autos.jsx
+++ ProntaEntregaReact/src/pages/Autos/Autos.jsx
@@ -28,11 +28,11 @@ function AutosComponent() {
     const [searchQuery, setSearchQuery] = useState('');
     const [orderCriteria, setOrderCriteria] = useState(null);
     const [formData, setFormData] = useState({
-        imagen: null,
-        "marca": "",
-        "modelo": "",
-        "patente": "",
-        "kilometros": "",
+        imagen: "",
+        marca: "",
+        modelo: "",
+        patente: "",
+        kilometros: "",
     });
 
     useEffect(() => {
@@ -212,7 +212,7 @@ function AutosComponent() {
                         <Modal openButtonText='¿No encuentra su auto? Añadalo' openButtonWidth='20' title='Nuevo Auto' saveButtonText='Crear' handleSave={handleCreateAuto}  content={
                             <div>
                                 <h2 className='centered'> Nuevo Auto </h2>
-                                <UploadImage wide='13' onFileChange={handleFileChange}/>
+                                <UploadImage name="imagen" wide='13' onFileChange={handleFileChange}/>
                                 <Form.Control name="marca" type="text" placeholder="Marca" onChange={handleInputChange} style={{ borderRadius: '10rem', backgroundColor: '#F5F5F5', boxShadow: '0.10rem 0.3rem 0.20rem rgba(0, 0, 0, 0.3)', marginTop: '1rem' }} />
                                 <Form.Control name="modelo" type="text" placeholder="Modelo" onChange={handleInputChange} style={{ borderRadius: '10rem', backgroundColor: '#F5F5F5', boxShadow: '0.10rem 0.3rem 0.20rem rgba(0, 0, 0, 0.3)', marginTop: '1rem' }} />
                                 <Form.Control name="patente" type="text" placeholder="Patente" onChange={handleInputChange} style={{ borderRadius: '10rem', backgroundColor: '#F5F5F5', boxShadow: '0.10rem 0.3rem 0.20rem rgba(0, 0, 0, 0.3)', marginTop: '1rem' }} />
