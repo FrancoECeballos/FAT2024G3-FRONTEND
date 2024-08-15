b89de90648495fe3f9f8e11ebc53370a1c33f150
WeigandtTimoteo <weigandttimoteo@gmail.com>
2024-08-15 10:14:43 -0300

    Cambios en OneProduct para que pitu los corrija por que pitu es un capo y un genio

--- ProntaEntregaReact/src/pages/Stock/OneProduct/OneProduct.jsx
+++ ProntaEntregaReact/src/pages/Stock/OneProduct/OneProduct.jsx
@@ -2,13 +2,38 @@ import React from "react";
 
 import FullNavbar from "../../../components/navbar/full_navbar/FullNavbar";
 import ProductCard from "../../../components/cards/product_card/ProductCard";
+import { useParams } from "react-router-dom";
+import fetchData from "../../../functions/fetchData";
+import Cookies from 'js-cookie';
 
 function OneProduct() {
+  const [product, setProduct] = React.useState(null);
+  const { productoId } = useParams();
+  const token = Cookies.get('token');
+
+  React.useEffect(() => {
+    console.log(productoId);
+    fetchProduct();
+    console.log(product);
+  }, []);
+
+  const fetchProduct = async () => {
+    try {
+      fetchData(`/producto/${productoId}`, token).then((result) => {
+        setProduct(result);
+      });
+    } catch (error) {
+      console.error("Error fetching product:", error);
+    }
+  };
+
   return (
     <div>
-        <FullNavbar/>
-        <ProductCard/>
-        <h1>OneProduct</h1>
+      <FullNavbar />
+      <ProductCard product={product} />
+      <h1>OneProduct</h1>
     </div>
   );
-} export default OneProduct;
+}
+
+export default OneProduct;
