3bd9a1db63385c0267ab5cae7cccabbfadf74135
MateoTempesti <m.tempesti.ruffino@itsv.edu.ar>
2024-09-13 10:22:14 -0300

    pwa en react

--- ProntaEntregaReact/vite.config.js
+++ ProntaEntregaReact/vite.config.js
@@ -1,7 +1,31 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg', 'robots.txt', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'Pronta Entrega',
+        short_name: 'PE',
+        description: 'Aplicación de Pronta Entrega',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'icons/prontalogo_192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'icons/prontalogo_512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
+});
---
+++ ProntaEntregaReact/webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { GenerateSW } = require('workbox-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.jsx', // Punto de entrada para la aplicación
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js', // Nombre del archivo de salida
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/, // Archivo a ser procesado
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'], // Extensiones a resolver
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html', // Plantilla HTML
+    }),
+    new GenerateSW({
+      clientsClaim: true,
+      skipWaiting: true,
+    }),
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, 'build'),
+    compress: true,
+    port: 3000,
+  },
+};
