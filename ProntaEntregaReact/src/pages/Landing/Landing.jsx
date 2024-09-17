8ca8f64aa7cfaa4001859fd3679d319a9e1ce30f
RenzoProsdo <renzo.prosdocimo.ps4@gmail.com>
2024-09-17 10:31:40 -0300

    landig resposive

--- ProntaEntregaReact/src/pages/Landing/Landing.jsx
+++ ProntaEntregaReact/src/pages/Landing/Landing.jsx
@@ -12,63 +12,76 @@ import foto2 from '../../assets/foto2.jpg';
 import foto3 from '../../assets/foto3.jpg';
 import './Landing.scss';
 
-
-function Landing (){
- 
+function Landing() {
   return (
     <div>
-      <BaseNavbar children={
-        <div style={{marginRight:'1.5rem'}}>
-          <SendButton text="Ingresar" wide="7" backcolor="#D9D9D9" letercolor="black" id="login" hid={false} shadow='none' href='/login'/>
+      <BaseNavbar>
+        <div style={{ marginRight: '1.5rem' }}>
+          <SendButton 
+            text="Ingresar" 
+            wide="7" 
+            backcolor="#D9D9D9" 
+            letercolor="black" 
+            id="login" 
+            hid={false} 
+            shadow="none" 
+            href="/login" 
+          />
         </div>
-      }/>
-      <ControlledCarousel foto1={foto1} foto2={foto2} foto3={foto3}/>
-      <br/>
-      <div style={{textAlign:'center',fontFamily: '', marginLeft:'10rem',marginRight:'10rem'}}>
-        <h1 style={{fontSize: '2.5rem'}}>¿Que es pronta entrega?</h1>
-        <p style={{fontSize: '1.25rem'}}>
-          Pronta Entrega busca brindar una forma de contactar a las 
-          <br/>
-          obras para distribuir recursos y agilizar la administración de estos materiales.
+      </BaseNavbar>
+      <ControlledCarousel foto1={foto1} foto2={foto2} foto3={foto3} />
+      <div className="section text-center">
+        <h1>¿Qué es Pronta Entrega?</h1>
+        <p>
+          Pronta Entrega busca brindar una forma de contactar a las obras para distribuir 
+          recursos y agilizar la administración de estos materiales.
         </p>
-        </div>
-        <br/>
-        <br/>
-        <div style={{textAlign:'center',fontFamily: '', marginLeft:'15rem',marginRight:'15rem'}}>   
-        <h1 style={{fontSize: '2.5rem'}}>¿Quienes somos?</h1>
-        <p style={{fontSize: '1.25rem'}}>Una fundación de inspiración jesuita abierta a toda persona de buena voluntad. A través del compromiso de voluntarios nos proponemos servir y
-           promover a los más necesitados mejorando su calidad de vida y suavizando las situaciones de pobreza, dolor y soledad, interviniendo en espacios que están desatendidos e ignorados en la sociedad.</p>
-        </div>
-        <br/>
-        <br/>
-        <div style={{paddingRight:'30rem',paddingLeft:'30rem'}}>
+      </div>
+
+      <div className="section text-center">
+        <h1>¿Quiénes somos?</h1>
+        <p>
+          Una fundación de inspiración jesuita abierta a toda persona de buena voluntad. A través 
+          del compromiso de voluntarios, nos proponemos servir y promover a los más necesitados, 
+          mejorando su calidad de vida y suavizando las situaciones de pobreza, dolor y soledad.
+        </p>
+      </div>
+
+      <div className="tabs-container">
         <Tabs defaultActiveKey="profile" id="fill-tab-example" className="mb-3" fill>
-        <Tab style={{textAlign:'center'}} eventKey="home" title="Stock">
-        <Icon style={{width:'10rem', height:'10rem',marginTop:'1.5rem',marginBottom:'1.5rem'}} icon="material-symbols:inventory-2-outline" />
-        <h1>Stock</h1>
-        <br/>
-        <p style={{fontSize: '1.8rem',marginTop:'1rem'}}>
-        Se proporcionará un sistema de registro del stock rápido y sencillo de utilizar para que los voluntarios puedan visualizar los recursos de sus obras, 
-        ingresar cambios fácilmente y recibir notificaciones en caso de exceso o escasez de un producto</p>
-        </Tab>
-        <Tab style={{textAlign:'center'}} eventKey="profile" title="Pedidos">
-        <Icon style={{width:'10rem', height:'10rem',marginTop:'1.5rem', marginBottom:'1.5rem'}} icon="bi:cart" />
-        <h1>Pedidos</h1>
-        <p style={{fontSize: '1.8rem',marginTop:'1.5rem'}}>
-        Basado en el sistema anterior, si se necesita o se tiene demasiado de un producto, un voluntario puede crear un pedido o una oferta que será vista por los integrantes de todas las obras. 
-        Este podrá ser tomado, lo que lo llevará al último apartado</p>
-        </Tab>
-        <Tab style={{textAlign:'center'}} eventKey="longer-tab" title="Envios">
-        <Icon style={{width:'10rem', height:'10rem',marginTop:'1.5rem', marginBottom:'1.5rem'}} icon="grommet-icons:deliver" />
-        <h1>Envios</h1>
-        <p style={{fontSize: '1.8rem',marginTop:'1.5rem'}}>
-        Cuando se reclama un pedido o una oferta, los voluntarios podrán acordar la fecha de entrega y la forma del traslado.
-        Finalmente, proporcionaremos una forma de almacenar los vehículos a disposición de la organización.</p>
-        </Tab>
+          <Tab eventKey="home" title="Stock">
+            <Icon className="tab-icon" icon="material-symbols:inventory-2-outline" />
+            <h1>Stock</h1>
+            <p>
+              Se proporcionará un sistema de registro del stock rápido y sencillo para que los 
+              voluntarios puedan visualizar los recursos de sus obras y recibir notificaciones 
+              en caso de exceso o escasez de productos.
+            </p>
+          </Tab>
+
+          <Tab eventKey="profile" title="Pedidos">
+            <Icon className="tab-icon" icon="bi:cart" />
+            <h1>Pedidos</h1>
+            <p>
+              Basado en el sistema anterior, los voluntarios pueden crear pedidos u ofertas que 
+              serán vistas por los integrantes de las obras. Este sistema agilizará la gestión de recursos.
+            </p>
+          </Tab>
+
+          <Tab eventKey="longer-tab" title="Envíos">
+            <Icon className="tab-icon" icon="grommet-icons:deliver" />
+            <h1>Envíos</h1>
+            <p>
+              Los voluntarios podrán acordar la fecha de entrega y la forma del traslado, proporcionando 
+              una forma de almacenar los vehículos a disposición de la organización.
+            </p>
+          </Tab>
         </Tabs>
-        </div>
-        <Footer/>
+      </div>
+
+      <Footer />
     </div>
   );
-};
+}
+
 export default Landing;
