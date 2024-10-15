e4913cdf2f0021c457e2e63e633f36f870c5f4be
Lucio Peralta <lucio.peralta.ar@gmail.com>
2024-10-15 09:15:19 -0300

    Added function UserIsAdmin

---
+++ ProntaEntregaReact/src/functions/userIsAdmin.jsx
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+const esAdminDeObra = async (obraId, usuarioId) => {
+    try {
+        const response = await axios.get(`/admin_obra/?id_obra=${obraId}&id_usuario=${usuarioId}`);
+        console.log('Respuesta del servidor:', response.data); // Agrega este log para verificar la respuesta
+        const detalle = response.data;
+        return detalle.es_admin; // Asegúrate de que este campo exista en la respuesta
+    } catch (error) {
+        console.error('Error al verificar si el usuario es admin de la obra:', error);
+        return false;
+    }
+};
+
+export default esAdminDeObra;
