1532daaac1e53fc38eb1be127c6b4dafccf6a22b
FrancoECeballos <franco.ceballos2005@gmail.com>
2024-10-31 10:48:47 -0300

    Updated crearNotificacion function

--- ProntaEntregaReact/src/functions/createNofiticacion.jsx
+++ ProntaEntregaReact/src/functions/createNofiticacion.jsx
@@ -1,9 +1,17 @@
 import postData from './postData';
 
-const crearNotificacion = async (notificacionData, authToken, id_usuario, id_obra) => {
+const crearNotificacion = async (notificacionData, authToken, operation = 'All', id) => {
     try {
-        const response = await postData('/PostNotificacion/', notificacionData, authToken);
-        return response;
+        if (operation === 'User') {
+            const response = await postData(`/PostNotificacionForUser/${id}/`, notificacionData, authToken);
+            return response;
+        } else if (operation === 'Obra') {
+            const response = await postData(`/PostNotificacionForObra/${id}/`, notificacionData, authToken);
+            return response;
+        } else {
+            const response = await postData(`/PostNotificacionForAll/`, notificacionData, authToken);
+            return response;
+        }
     } catch (error) {
         console.error('Error creando notificación:', error);
         throw error;
