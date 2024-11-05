2eab6542e4fd03c8df172d00c657e6f39328d5bb
RenzoProsdo <renzo.prosdocimo.ps4@gmail.com>
2024-11-05 08:58:57 -0300

    [200~👩‍❤️‍💋‍👩~

--- ProntaEntregaReact/src/components/cards/oferta_listing_card/OfertaListingCard.jsx
+++ ProntaEntregaReact/src/components/cards/oferta_listing_card/OfertaListingCard.jsx
@@ -1,11 +1,9 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { Card, Carousel } from "react-bootstrap";
 import Cookies from 'js-cookie';
 import Loading from '../../loading/loading';
-
 import fetchUser from '../../../functions/fetchUser';
 import fetchData from "../../../functions/fetchData";
-
 import './OfertaListingCard.scss';
 
 function OfertaListingCard() {
@@ -14,22 +12,20 @@ function OfertaListingCard() {
   const token = Cookies.get('token');
   const [isLoading, setIsLoading] = useState(true);
 
-
   useEffect(() => {
     const fetchDataAsync = async () => {
-        try {
-            const userData = await fetchUser();
-            setUser(userData);
+      try {
+        const userData = await fetchUser();
+        setUser(userData);
 
-            const ofertas = await fetchData(`GetOfertasRecientes/${userData.id_usuario}`, token);
-            setOfertas(ofertas);
-            console.log(ofertas);
-            
-        } catch (error) {
-            console.error('Error fetching data:', error);
-        } finally {
-            setIsLoading(false);
-        }
+        const ofertas = await fetchData(`GetOfertasRecientes/${userData.id_usuario}`, token);
+        setOfertas(ofertas);
+        console.log(ofertas);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      } finally {
+        setIsLoading(false);
+      }
     };
     fetchDataAsync();
   }, [token]);
@@ -37,47 +33,42 @@ function OfertaListingCard() {
   return (
     <div>
       {isLoading ? (
-        <Card className="ol-card w-100 h-100" style={{minWidth:'100%', minHeight:'100%'}}>
+        <Card className="ol-card w-100 h-100" style={{ minWidth: '100%', minHeight: '100%' }}>
           <Loading />
         </Card>
       ) : (
-        <Card className="ol-card w-100 h-100 centered">
-            <Card.Title>Ofertas Recientes <hr /></Card.Title>
-            <Card.Body>
+        <Card className="ol-card w-100 h-100">
+          <Card.Title>Ofertas Recientes <hr /></Card.Title>
+          <Card.Body>
             <Carousel>
               {Array.isArray(ofertas) && ofertas.length > 0 ? (
-                ofertas.map((oferta, index) => (  
+                ofertas.map((oferta, index) => (
                   <Carousel.Item key={index}>
-                    <div style={{ display: 'flex', alignItems: 'center' }}>
-                        <img 
-                            src={oferta.id_producto.imagen} 
-                            alt="" 
-                            style={{
-                                width: '6rem',
-                                height: '6rem',
-                                borderRadius: '1rem',
-                                objectFit: 'scale-down'
-                            }}
-                        />
-                        <div className="ol-derecha">
-                            <div className="text-content">
-                                <Card.Title>{oferta.id_obra.nombre}</Card.Title>
-                                <Card.Text>{oferta.id_producto.descripcion}</Card.Text>
-                                <Card.Text>{oferta.fechavencimiento}</Card.Text>
-                            </div>
+                    <div className="carousel-item-content">
+                      <img
+                        src={oferta.id_producto.imagen}
+                        alt=""
+                        className="carousel-image"
+                      />
+                      <div className="ol-derecha">
+                        <div className="text-content">
+                          <Card.Title>{oferta.id_obra.nombre}</Card.Title>
+                          <Card.Text>{oferta.id_producto.descripcion}</Card.Text>
+                          <Card.Text>{oferta.fechavencimiento}</Card.Text>
                         </div>
+                      </div>
                     </div>
-                </Carousel.Item>
-
+                  </Carousel.Item>
                 ))
-              ):(
+              ) : (
                 <Card.Text>No hay ofertas</Card.Text>
               )}
-              
             </Carousel>
-            </Card.Body>
+          </Card.Body>
         </Card>
       )}
     </div>
   );
-} export default OfertaListingCard;
+}
+
+export default OfertaListingCard;
