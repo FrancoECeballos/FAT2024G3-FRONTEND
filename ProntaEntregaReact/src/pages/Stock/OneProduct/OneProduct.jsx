3c8fb35a877b651efd976457f96400bd6b97acb6
Chialvo <lautarochialvo2005@gmail.com>
2024-11-07 09:40:15 -0300

    fix OneProduct

--- ProntaEntregaReact/src/pages/Stock/OneProduct/OneProduct.jsx
+++ ProntaEntregaReact/src/pages/Stock/OneProduct/OneProduct.jsx
@@ -1,71 +1,53 @@
-import React, { useState, useEffect } from 'react';
-import { Table } from 'react-bootstrap';
-import { useParams } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import FullNavbar from "../../../components/navbar/full_navbar/FullNavbar";
+import ProductCard from "../../../components/cards/product_card/ProductCard";
+import SendButton from "../../../components/buttons/send_button/send_button";
+import Loading from "../../../components/loading/loading.jsx";
+import BackButton from '../../../components/buttons/back_button/back_button';
+
+import { Table } from "react-bootstrap";
+import './OneProduct.scss';
+
+import { useParams } from "react-router-dom";
+import fetchData from "../../../functions/fetchData";
 import Cookies from 'js-cookie';
 
-import FullNavbar from '../../../components/navbar/full_navbar/FullNavbar.jsx';
-import BackButton from '../../../components/buttons/back_button/back_button.jsx';
-import ProductCard from '../../../components/cards/product_card/ProductCard.jsx';
-import Loading from '../../../components/loading/loading.jsx';
-import SearchBar from '../../../components/searchbar/searchbar.jsx';
+import { useNavigate } from 'react-router-dom';
 
-const OneProduct = () => {
+function OneProduct() {
+  const navigate = useNavigate();
+  const [detallesProduct, setDetallesProduct] = useState([]);
+  const [product, setProduct] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
   const { productoId, stockId, categoriaID } = useParams();
   const token = Cookies.get('token');
-  const [product, setProduct] = useState(null);
-  const [detallesProduct, setDetallesProduct] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [searchQuery, setSearchQuery] = useState('');
-  const [order, setOrder] = useState('');
 
   useEffect(() => {
+    if (!token) {
+        navigate('/login');
+        return;
+    }
+    
     const fetchProductData = async () => {
-      try {
-        const productResult = await fetchData(`GetProducto/${productoId}/`, token);
-        setProduct(productResult);
-
         try {
-          const detallesResult = await fetchData(`GetDetallesProductoObra/${productoId}/${stockId}/`, token);
-          setDetallesProduct(detallesResult);
+            const productResult = await fetchData(`/producto/${parseInt(productoId, 10)}`, token);
+            setProduct(productResult[0]);
+
+            try {
+                const detallesResult = await fetchData(`GetDetallesProductoObra/${productoId}/${stockId}/`, token);
+                setDetallesProduct(detallesResult);
+            } catch (error) {
+                console.error("Error fetching product details:", error);
+            }
         } catch (error) {
-          console.error("Error fetching product details:", error);
+            console.error("Error fetching product:", error);
+        } finally {
+            setIsLoading(false);
         }
-      } catch (error) {
-        console.error("Error fetching product:", error);
-      } finally {
-        setIsLoading(false);
-      }
     };
 
     fetchProductData();
   }, [productoId, stockId, token]);
-
-  const handleSearchChange = (query) => {
-    setSearchQuery(query);
-  };
-
-  const handleOrderChange = (order) => {
-    setOrder(order);
-  };
-
-  const filteredData = detallesProduct.filter(detalleProduct => {
-    return (
-      detalleProduct.id_detallestockproducto.toString().includes(searchQuery) ||
-      detalleProduct.cantidad.toString().includes(searchQuery) ||
-      detalleProduct.fecha_creacion.toLowerCase().includes(searchQuery) ||
-      (detalleProduct.id_usuario?.nombre + ' ' + detalleProduct.id_usuario?.apellido).toLowerCase().includes(searchQuery)
-    );
-  });
-
-  const sortedData = [...filteredData].sort((a, b) => {
-    if (!order) return 0;
-    const [key, direction] = order.split(' ');
-    const aValue = key.split('.').reduce((acc, key) => acc && acc[key], a);
-    const bValue = key.split('.').reduce((acc, key) => acc && acc[key], b);
-    if (aValue < bValue) return direction === 'asc' ? -1 : 1;
-    if (aValue > bValue) return direction === 'asc' ? 1 : -1;
-    return 0;
-  });
 
   if (isLoading) {
     return <Loading />;
@@ -74,22 +56,10 @@ const OneProduct = () => {
   return (
     <div>
       <FullNavbar selectedPage={'Stock'}/>
-      <div className="product-page">
-        <BackButton url={`/obra/${stockId}/categoria/${categoriaID}/`}/>
-        <ProductCard product={product} />
-        <div style={{ textAlign: 'center' }}>
-          <SearchBar 
-            onSearchChange={handleSearchChange} 
-            onOrderChange={handleOrderChange} 
-            filters={[
-              { type: 'id_detallestockproducto asc', label: 'ID' },
-              { type: 'cantidad asc', label: 'Cantidad' },
-              { type: 'fecha_creacion asc', label: 'Fecha de Creación' },
-              { type: 'id_usuario.nombre + id_usuario.apellido asc', label: 'Usuario' },
-            ]}
-            style={{ width: '80rem', marginBottom: '1rem' }}
-          />
-          <Table style={{ margin: '0 auto', width: '95%', marginBottom:'2rem' }} striped bordered hover responsive className="custom-table mt-4">
+        <div className="product-page">
+          <BackButton url={`/obra/${stockId}/categoria/${categoriaID}/`}/>
+          <ProductCard product={product} />
+          <Table striped bordered hover responsive className="custom-table mt-4">
             <thead>
               <tr>
                 <th>#</th>
@@ -99,8 +69,9 @@ const OneProduct = () => {
               </tr>
             </thead>
             <tbody>
-              {sortedData.map((detalleProduct, index) => {
+              {detallesProduct.map((detalleProduct, index) => {
                 return (
+                  console.log(detalleProduct),
                   <tr key={index}>
                     <td>{detalleProduct.id_detallestockproducto}</td>
                     <td>{detalleProduct.cantidad} {detalleProduct.id_producto.unidadmedida}</td>
@@ -111,10 +82,13 @@ const OneProduct = () => {
               })}
             </tbody>
           </Table>
+          <SendButton
+            text="Descargar informe"
+            onClick={() => window.location.href = `http://127.0.0.1:8000/informe-stock-pdf/${productoId}/${stockId}/${token}`}
+          />
         </div>
-      </div>
     </div>
   );
-};
+}
 
 export default OneProduct;
